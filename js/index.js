b27d15e924a042bc62f24bc83db7841f8bd7918e
Damian Peiris <drpeiris3@gmail.com>
2023-06-25 21:50:04 +0530

    Fixed CSS in lyric viewer.

--- js/index.js
+++ js/index.js
@@ -7,14 +7,13 @@ $("#nameField").on("keydown", (event) => {
 });
 
 async function fetchSongID(songName) {
-    $(".lyricsContainer").empty();
-    $(".albumCover").empty();
-    $("#albumImage").remove();
+    $(".albumDetails").remove();
     $("#nameField").val("");
     $("#nameField").blur();
     let response = await fetch("https://api.genius.com/search?access_token=H4jUebTWrLHmEM_g0L1gfBu3ZUgDpamrZAuwy9xvBWfNysQ5yBJ5CdCGUrgrXEa3&q=" + songName)
     let jsonData = response.json();
     let musicData = await jsonData;
+    $("body").append("<div class='albumDetails' ></div>")
     $(".albumDetails").append("<h1 data-aos='zoom-in' >" + musicData.response.hits[0].result.full_title + ".</h1>")
     $(".albumDetails").append("<h1 data-aos='zoom-in' >"+ "Released on :" + musicData.response.hits[0].result.release_date_for_display + ".</h1>")
     var albumCover = musicData.response.hits[0].result.header_image_url;
@@ -30,7 +29,8 @@ async function fetchSongID(songName) {
 if (window.innerWidth < 878) {
     $(".heading").css("font-size", "3rem");
     $("#nameField").attr("placeholder", "Enter Song Name!");
-    $(".albumCover>h1").css("font-size", "1rem");
+    $(".albumDetails>h1").css("font-size", "1rem");
+    $(".albumDetails>image").css("width", "80vw");
     $(".intro").css("font-size", "2rem");
 
 }
