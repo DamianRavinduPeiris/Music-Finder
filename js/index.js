f3d4b33ae0159b7908f11f2038fe00e4d9b4649c
Damian Peiris <drpeiris3@gmail.com>
2023-06-25 13:47:58 +0530

    Changed the landing page view.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,44 @@
-$("#nameField").on("keydown",(event)=>{
-    if(event.key==="Enter"){
+$(document).ready(() => {
+    window.onload = function () {
+        Particles.init({
+            selector: '.background'
+        });
+    };
+});
+$("#nameField").on("keydown", (event) => {
+    if (event.key === "Enter") {
         fetchSongID($("#nameField").val());
 
 
     }
 });
-async function fetchSongID(songName){
+
+async function fetchSongID(songName) {
     $(".albumCover").empty();
     $("#albumImage").remove();
-    $("#nameField").val();
+    $("#nameField").val("");
     $("#nameField").blur();
-    let response = await fetch("https://api.genius.com/search?access_token=H4jUebTWrLHmEM_g0L1gfBu3ZUgDpamrZAuwy9xvBWfNysQ5yBJ5CdCGUrgrXEa3&q="+songName)
-    let jsonData = await  response.json();
+    let response = await fetch("https://api.genius.com/search?access_token=H4jUebTWrLHmEM_g0L1gfBu3ZUgDpamrZAuwy9xvBWfNysQ5yBJ5CdCGUrgrXEa3&q=" + songName)
+    let jsonData = await response.json();
     let musicData = await jsonData;
-    $(".albumCover").append("<h1 data-aos='zoom-in'>"+musicData.response.hits[0].result.full_title+".</h1>")
+    $(".albumCover").append("<h1 data-aos='zoom-in' >" + musicData.response.hits[0].result.full_title + ".</h1>")
     var albumCover = musicData.response.hits[0].result.header_image_url;
-    $("body").append("<img data-aos='zoom-in' id='albumImage' src="+albumCover+">")
+    var sId = musicData.response.hits[0].result.id;
+    fetchLyrics(sId);
+    $("body").append("<img  id='albumImage' src=" + albumCover + ">")
+    /*Do not scroll horizontally (Indicates by 0 , Scroll to the vertical position. )*/
+    window.scrollTo(0, document.body.scrollHeight);
+
+}
+
+if (window.innerWidth < 878) {
+    $(".heading").css("font-size", "3rem");
+    $("#nameField").attr("placeholder", "Enter Song Name!");
+    $(".albumCover>h1").css("font-size", "1rem");
+
+}
+
+async function fetchLyrics(songId) {
+
 
 }
